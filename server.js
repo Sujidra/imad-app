03911ace5008eba3f02df9804d4007ff97d8d30b
Sujidra <30981889+Sujidra@users.noreply.github.com>
2017-09-09 14:43:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ var config={
     port:'5432',
     password:process.env.DB_PASSWORD
 };
-var pool = new Pool(config);
+var pool = new pool(config);
 app.get('/test-db',function(req,res){
     pool.query('select * from user',function(err,result)
     {
